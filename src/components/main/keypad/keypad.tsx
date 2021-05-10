742c3275aa24b1c4d552b43c4ec952affc9af4c0
ascolm <mertascioglu@gmail.com>
2021-05-10 11:50:32 +0300

    feat(main): separate display and current values, add useEffect hook for display

--- src/components/main/keypad/keypad.tsx
+++ src/components/main/keypad/keypad.tsx
@@ -3,19 +3,20 @@ import Button from './button/button';
 
 export interface Props {
   numberHandler: (value: string) => void;
+  operatorHandler: (value: string) => void;
 }
 
-const Keypad: React.FC<Props> = ({ numberHandler }) => {
+const Keypad: React.FC<Props> = ({ numberHandler, operatorHandler }) => {
   return (
     <div className={styles.keypadContainer}>
       <Button value='7' clickHandler={numberHandler}/>
       <Button value='8' clickHandler={numberHandler}/>
       <Button value='9' clickHandler={numberHandler}/>
-      <Button value='+' clickHandler={numberHandler}/>
+      <Button value='+' clickHandler={operatorHandler}/>
       <Button value='4' clickHandler={numberHandler}/>
       <Button value='5' clickHandler={numberHandler}/>
       <Button value='6' clickHandler={numberHandler}/>
-      <Button value='-' clickHandler={numberHandler}/>
+      <Button value='-' clickHandler={operatorHandler}/>
       <Button value='1' clickHandler={numberHandler}/>
       <Button value='2' clickHandler={numberHandler}/>
       <Button value='3' clickHandler={numberHandler}/>
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,24 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './main.module.scss';
 import Display from 'components/main/display/display';
 import Keypad from 'components/main/keypad/keypad';
 
 const Main: React.FC = () => {
   let [displayedValue, setDisplayedValue] = useState<string>('0');
+  let [currentValue, setCurrentValue] = useState<string | null>(null);
   let [memoryValue, setMemoryValue] = useState<string | null>(null);
   let [activeOperator, setActiveOperator] = useState<string>('');
 
+  useEffect(() => {
+    if (currentValue !== null) setDisplayedValue(currentValue);
+  }, [currentValue]);
+
   function numberButtonHandler (value: string) {
-    if (displayedValue === '0' && memoryValue === null) {
-      setDisplayedValue(value);
+    if (currentValue === null) {
+      setCurrentValue(value);
     } else {
-      setDisplayedValue(valueOnDisplay =>  valueOnDisplay + value);
+      setCurrentValue(valueOnDisplay =>  valueOnDisplay + value);
     }
   }
+
+  function operatorButtonHandler (value: string) {
+    if (!activeOperator) {
+      setActiveOperator(value);
+      setMemoryValue(currentValue);
+      setCurrentValue(null);
+    }
+  }
+
   return (
     <div className={styles.mainContainer}>
       <Display displayedValue={displayedValue}/>
-      <Keypad numberHandler={numberButtonHandler}/>
+      <Keypad numberHandler={numberButtonHandler} operatorHandler={operatorButtonHandler}/>
     </div>
   );
 };
