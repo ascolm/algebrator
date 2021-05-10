14f53a3a03c70ddb4a1d8d1b151790d94a56d4c6
ascolm <mertascioglu@gmail.com>
2021-05-10 12:14:21 +0300

    feat(main): implement operator handler and calculate function

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -5,16 +5,20 @@ import Keypad from 'components/main/keypad/keypad';
 
 const Main: React.FC = () => {
   let [displayedValue, setDisplayedValue] = useState<string>('0');
-  let [currentValue, setCurrentValue] = useState<string | null>(null);
-  let [memoryValue, setMemoryValue] = useState<string | null>(null);
+  let [currentValue, setCurrentValue] = useState<string>('0');
+  let [memoryValue, setMemoryValue] = useState<string>('0');
   let [activeOperator, setActiveOperator] = useState<string>('');
 
   useEffect(() => {
-    if (currentValue !== null) setDisplayedValue(currentValue);
+    if (currentValue !== '0') {
+      setDisplayedValue(currentValue);
+    } else if (memoryValue !== '0') {
+      setDisplayedValue(memoryValue);
+    }
   }, [currentValue]);
 
   function numberButtonHandler (value: string) {
-    if (currentValue === null) {
+    if (currentValue === '0') {
       setCurrentValue(value);
     } else {
       setCurrentValue(valueOnDisplay =>  valueOnDisplay + value);
@@ -25,7 +29,23 @@ const Main: React.FC = () => {
     if (!activeOperator) {
       setActiveOperator(value);
       setMemoryValue(currentValue);
-      setCurrentValue(null);
+    } else {
+      setActiveOperator(value);
+      setMemoryValue(valueInMemory => '' + calculate(valueInMemory, activeOperator, currentValue));
+    }
+
+    setCurrentValue('0');
+  }
+
+  function calculate (valueL: string, operator: string, valueR: string) {
+    const valueLeft = parseFloat(valueL);
+    const valueRight = parseFloat(valueR);
+
+    switch (operator) {
+    case ('+'):
+      return valueLeft + valueRight;
+    case ('-'):
+      return valueLeft - valueRight;
     }
   }
 
